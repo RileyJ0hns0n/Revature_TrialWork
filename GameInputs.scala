905e373e9c2a6d985982712c7db9f89a182092d6
RileyJ0hns0n <100624592+RileyJ0hns0n@users.noreply.github.com>
2022-03-08 20:25:27 -0500

    Add files via upload
    I want to check if this is working

---
+++ GameInputs.scala
@@ -0,0 +1,23 @@
+class GameInputs {
+  //Rock Paper Scissors (potential: Lizard,Spock)
+
+  //Valid Moves
+    //1 === Rock\n
+
+    //2 === Paper\n
+
+    //3 === Scissors\n
+
+    //4 === Lizard\n
+
+    //5 === Spock\n
+
+      //Store What Everyone Throws/ History
+
+
+  //allow new move if previous is invalid
+
+  //CPU Selects a Random Int between 1-3 (or 1-5)
+
+  //Quit or Continue Option
+}
---
+++ GameRules.scala
@@ -0,0 +1,62 @@
+import scala.io.StdIn._
+import java.util._
+import java.io._
+import java.util.Scanner._
+
+class GameRules {
+  //Big Bang RPS Game Rules
+
+    //Begin new Game
+      //Generate Questions
+      // "What is your name? " readString
+
+  val playerFirstname: String = readLine()
+
+  println(s"Your name is: $playerFirstname")
+
+        //"Have You Played Before? " (y/n) Boolean
+
+          /*"Rules Are:
+            Rock Breaks Scissors,
+            Rock Crushes Lizard,
+            Scissors Cuts Paper,
+            Scissors Decap Lizard,
+            Paper Covers Rock,
+            Paper Disproves Spock,
+            Lizard Eats Paper,
+            Lizard Poisons Spock
+            Spock Breaks Scissors,
+            Spock Vaporizes Rock,
+            Tied scenario: No Winner or Looser*/
+
+    //Rock Breaks Scissors
+      //Player(x) has won the game
+
+    //Rock Crushes Lizard
+
+    //Scissors Cuts Paper
+
+    //Scissors Decaps Lizard
+
+    //Paper Covers Rock
+
+    //Paper Disproves Spock
+
+    //Lizard Eats Paper
+
+    //Lizard Poisons Spock
+
+    //Spock Breaks Scissors
+
+    //Spock Vaporizes Rock
+
+    //Tied scenario
+
+
+    //Reset Game State
+
+    //Begin new Game
+}
+      /*have a table of all kinds of moves to make.
+      I.e. A skill table that can be expanded by vague type
+       */
